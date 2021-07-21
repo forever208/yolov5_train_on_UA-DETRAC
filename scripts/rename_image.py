75810ad84324d2f63b885e8d02646546b7683e7e
forever208 <ningmang666@gmail.com>
2021-07-21 12:28:25 +0200

    modify path for Colab

--- scripts/rename_image.py
+++ scripts/rename_image.py
@@ -8,58 +8,55 @@ import shutil
 import re
 import sys
 
-FLAG = 0
-if FLAG == 1:
 
-    ##train
-    src_dir = "/home/lj/zzg/fenghuo-zzg/DETRAC-dataset/Images/Insight-MVT_Annotation_Train/" #image
-    dst_dir = "/home/lj/zzg/fenghuo-zzg/DETRAC-dataset/Annotations/0510/scripts/train_image"
+for FLAG in range(2):
+    if FLAG == 0:
+        ##train
+        src_dir = "/content/Insight-MVT_Annotation_Train/"  # image
+        dst_dir = "/content/DETRAC-dataset/Annotations/0510/scripts/train_image"
+    else:
+        ##test
+        src_dir = "/content/Insight-MVT_Annotation_Test/"  # image
+        dst_dir = "/content/DETRAC-dataset/Annotations/0510/scripts/test_image"
 
-else:
-    ##test
-    src_dir = "/home/lj/zzg/fenghuo-zzg/DETRAC-dataset/Images/Insight-MVT_Annotation_Test/" #image
-    dst_dir = "/home/lj/zzg/fenghuo-zzg/DETRAC-dataset/Annotations/0510/scripts/test_image"
+    if not osp.exists(dst_dir):
+        os.makedirs(dst_dir)
 
-def mkdirs(d):
-    if not osp.exists(d):
-        os.makedirs(d)
+    seqs = [s for s in os.listdir(src_dir)]
 
-mkdirs(dst_dir)
+    for seq in seqs:
+        path = osp.join(src_dir, seq)
+        # print(path)
+        fileList = os.listdir(path)
+        os.chdir(path)
 
-seqs = [s for s in os.listdir(src_dir)]
+        for fileName in fileList:
+            pat = ".+\.(jpg|jpeg|JPG)"
+            pattern = re.findall(pat, fileName)
+            image_name = fileName.split(".")[0]
 
-for seq in seqs: 
-    path = osp.join(src_dir, seq)
-    # print(path)
-    fileList = os.listdir(path)  
-    os.chdir(path)  
+            os.rename(fileName, ('{}_{}.jpg'.format(image_name, str(seq))))
 
-    for fileName in fileList: 
-        pat = ".+\.(jpg|jpeg|JPG)"  
-        pattern = re.findall(pat, fileName)  
-        image_name = fileName.split(".")[0]  
+        sys.stdin.flush()
+        print("after rename：" + str(os.listdir(path)))
 
-        os.rename(fileName, ('{}_{}.jpg'.format(image_name, str(seq)))) 
-       
-    sys.stdin.flush()  
-    print("after rename：" + str(os.listdir(path))) 
+    print("step1 finished!-----")
 
-print("step1 finished!-----")
+    j = 0
+    for seq in seqs:
+        path = osp.join(src_dir, seq)
 
-j = 0
-for seq in seqs: 
-    path = osp.join(src_dir, seq)
+        for root, dirs, files in os.walk(path):
+            files = sorted(files)
+            for i in range(len(files)):
+                if i%10== 0:
+                    j += 1
+                    if (files[i][-3:] == 'jpg' or files[i][-3:] == 'JPG') or files[i][-4:]=='jpeg':
+                        file_path = path + '/' + files[i]
+                        new_file_path = dst_dir + '/' + files[i]
+                        shutil.copy(file_path, new_file_path)
 
-    for root, dirs, files in os.walk(path):
-        files = sorted(files)
-        for i in range(len(files)):
-            if i%10== 0: 
-                j += 1
-                if (files[i][-3:] == 'jpg' or files[i][-3:] == 'JPG') or files[i][-4:]=='jpeg':
-
-                    file_path = path + '/' + files[i]
-                    new_file_path = dst_dir + '/' + files[i]
-                    shutil.copy(file_path, new_file_path)
-                print(j)
-print("step2 finished!------")
-# print(str(os.listdir(dst_dir)))
+                    if i % 100 == 0:
+                        print(j)
+    print("step2 finished!------")
+    # print(str(os.listdir(dst_dir)))
